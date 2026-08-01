ca508aa5c7e757c6c214a8b62ad53def357f0fa3
Weeratat Kwandee <windyman18@gmail.com>
2026-08-01 10:56:43 +0700

    Add DbSets for categories and social activities

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SocialCharge.Models;
 
 namespace SocialCharge.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+public class ApplicationDbContext(
+    DbContextOptions<ApplicationDbContext> options)
+    : IdentityDbContext(options)
 {
+    public DbSet<Category> Categories { get; set; } = default!;
+
+    public DbSet<SocialActivity> SocialActivities { get; set; } = default!;
 }
